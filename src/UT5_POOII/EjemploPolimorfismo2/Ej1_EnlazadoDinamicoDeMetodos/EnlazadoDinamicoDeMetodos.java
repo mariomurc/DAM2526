6b49a2d6bdca60e7fb2dfcdbc0c684df2a365206
Mario <mariomurc@gmail.com>
2026-02-24 21:48:10 +0100

    Más Ejemplos Polimorfismo

---
+++ src/UT5_POOII/EjemploPolimorfismo2/Ej1_EnlazadoDinamicoDeMetodos/EnlazadoDinamicoDeMetodos.java
@@ -0,0 +1,31 @@
+package UT5_POOII.EjemploPolimorfismo2.Ej1_EnlazadoDinamicoDeMetodos;
+public class EnlazadoDinamicoDeMetodos {
+    public static void main(String[] args) {
+        metodo(new EstudianteGraduado());
+        metodo(new Estudiante());
+        metodo(new Persona());
+        Persona p=new Persona();
+        metodo(new Object());
+        System.out.println(p.getClass());
+    }
+
+    public static void metodo(Object o) {
+        System.out.println(o.toString());
+    }
+}
+class Persona extends Object {
+    @Override
+    public String toString() {
+        return "Persona";
+    }
+}
+class Estudiante extends Persona {
+    @Override
+    public String toString() {
+        return "Estudiante";
+    }
+}
+
+class EstudianteGraduado extends Estudiante {
+
+}
