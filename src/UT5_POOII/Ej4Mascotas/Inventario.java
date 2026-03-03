77eb845e0e0156f1313ca42aae3d4a25927b0bae
mario <mariomurc@gmail.com>
2026-03-03 10:57:21 +0100

    Ejercicio Aves (sin terminar)

---
+++ src/UT5_POOII/Ej4Mascotas/Inventario.java
@@ -0,0 +1,89 @@
+package UT5_POOII.Ej4Mascotas;
+
+import java.util.ArrayList;
+
+public class Inventario {
+
+    public Mascota[] arrayMascotas = new Mascota[5];
+    private int totalMascotas=4;
+    // Devuelve un String con el tipo de Animal
+    private String getTipoMascota(Mascota m) {
+        if (m instanceof Perro) {
+            return "Perro";
+        } else if (m instanceof Gato) {
+            return "Gato";
+        } else if (m instanceof Loro) {
+            return "Loro";
+        } else if (m instanceof Canario) {
+            return "Canario";
+        } else {
+            return null;
+        }
+    }
+
+    // Muestra la lista de animales (solo tipo y nombre)
+    public void mostrarListaAnimales() {
+        for (int i = 0; i < arrayMascotas.length; i++) {
+            if (arrayMascotas[i]!= null){
+                if (arrayMascotas[i] instanceof Perro){
+                    System.out.println("TIPO: PERRO");
+                    System.out.println( arrayMascotas[i].getNombre());
+                } else if (arrayMascotas[i] instanceof Gato) {
+                    System.out.println("TIPO: GATO ");
+                    System.out.println( arrayMascotas[i].getNombre());
+                } else if (arrayMascotas[i] instanceof Loro){
+                    System.out.println("TIPO: LORO");
+                    System.out.println( arrayMascotas[i].getNombre());
+                } else if (arrayMascotas[i] instanceof Canario) {
+                    System.out.println("TIPO: CANARIO");
+                    System.out.println( arrayMascotas[i].getNombre());
+                }
+                System.out.println("-------------------------------");
+            }
+        }
+
+    }
+    public void insertarAnimal(Mascota mascota) {
+        if (arrayMascotas != null) {
+            arrayMascotas[totalMascotas] = mascota;
+            totalMascotas++;
+        }
+    }
+
+    void mostrarDatosDeAnimal(Class<?> tipo){
+            for (int i = 0; i < arrayMascotas.length; i++) {
+                if (arrayMascotas[i] != null) {
+                    if(tipo.equals(arrayMascotas[i].getClass())){
+                        arrayMascotas[i].muestra();
+                    }
+                }
+            }
+
+        }
+
+    public void mostrarTodosAnimales() {
+        for (int i = 0; i < arrayMascotas.length; i++) {
+            if (arrayMascotas[i] != null) {
+                arrayMascotas[i].muestra();
+
+            }
+        }
+    }
+/*
+    public void insertarAnimal(Mascota a) {
+        animales.add(a);
+    }
+
+    public boolean eliminarAnimal(int indice) {
+        if (indice >= 0 && indice < animales.size()) {
+            animales.remove(indice);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public void vaciarInventario() {
+        animales.clear();
+    }*/
+}
--- src/UT5_POOII/Ej4Mascotas/ProgramaInventario.java
+++ src/UT5_POOII/Ej4Mascotas/ProgramaInventario.java
@@ -1,29 +1,42 @@
 package UT5_POOII.Ej4Mascotas;
 
 import java.time.LocalDate;
+import java.util.Scanner;
 
 public class ProgramaInventario {
     
     public static void main(String args[]) {
-
-        Inventario.inventMascotas[0] = new Perro("Bobby", LocalDate.of(2015,01,01),
+        Inventario inv = new Inventario();
+        inv.arrayMascotas[0] = new Perro("Bobby", LocalDate.of(2015,01,01),
                 "Pastor", false);
-        Inventario.inventMascotas[1] = new Gato("Pecas", LocalDate.of(2022,01,01),
+
+        inv.arrayMascotas[1] = new Gato("Pecas", LocalDate.of(2022,01,01),
                 "rojo", true);
-        Inventario.inventMascotas[2] = new Loro("Arturito", LocalDate.of(2024,01,01),
+        inv.arrayMascotas[2] = new Loro("Arturito", LocalDate.of(2024,01,01),
                 "largo", true, "Murcia", true);
-        Inventario.inventMascotas[3] = new Canario("Basilio", LocalDate.of(2022,01,01),
+        inv.arrayMascotas[3] = new Canario("Basilio", LocalDate.of(2022,01,01),
                 "Largo", true, "amarillo", true);
 
-
-        //Mostrar la lista de animales (solo tipo y nombre, 1 línea por animal).
-
+       //Mostrar la lista de animales (solo tipo y nombre, 1 línea por animal).
+        inv.mostrarListaAnimales();
         //Mostrar todos los datos de un animal concreto.
-
+      //  Scanner teclado = new Scanner(System.in);
+      //  System.out.println("De que animal quieres que te muestre info");
+      //  String a = teclado.nextLine();
+        System.out.println("**********************************");
+        System.out.println("**********************************");
+        System.out.println("**********************************");
+       inv.mostrarDatosDeAnimal(Perro.class);
+        System.out.println("**********************************");
+        System.out.println("**********************************");
+        System.out.println("**********************************");
         //Mostrar todos los datos de todos los animales.
-
+        inv.mostrarTodosAnimales();
         //Insertar animales en el inventario.
-
+        inv.insertarAnimal(new Canario("Basilio1", LocalDate.of(2022,01,01),
+                "Largo1", true, "amarillo1", true));
+        System.out.println("##########################");
+        inv.mostrarTodosAnimales();
         //Eliminar animales del inventario.
 
         //Vaciar
