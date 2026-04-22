79983e5563197434d611c196fce6f35225faaaea
mario <mariomurc@gmail.com>
2026-04-22 10:24:45 +0200

    UT8_AccesoABBDD

--- src/UT8_AccesoABBDD/MainBBDD.java
+++ src/UT8_AccesoABBDD/MainBBDD.java
@@ -1,11 +1,12 @@
 package UT8_AccesoABBDD;
 
 import java.sql.*;
+import java.util.ArrayList;
 
 public class MainBBDD {
     public static Connection conexion(String url, String user, String pass)throws SQLException{
 
-        Connection conn = DriverManager.getConnection(url, "alumno", "alumno");
+        Connection conn = DriverManager.getConnection(url, user, pass);
         return conn;
     }
 
@@ -14,26 +15,37 @@ public class MainBBDD {
         String url = "jdbc:mysql://localhost:3306/DAM1?allowPublicKeyRetrieval=true&useSSL=false&serverTimezone=UTC";
         String user = "alumno";
         String password = "alumno";
+        ArrayList<Productos> listaProductos = new ArrayList<>();
         /// FASE DE CONEXIÓN
-        Connection conn=null;
-        try {
-            conn = conexion(url,user, password);
-        } catch (SQLException e) {
+
+        //String sql = "SELECT nombre as n, descripcion, precio FROM productos";
+        String sql = "SELECT nombre,  descripcion, precio " +
+                "FROM productos " +
+                "WHERE precio > ? AND precio < ? "+
+                "ORDER BY ? ASC";
+        try (Connection conn = conexion(url,user, password);
+             PreparedStatement stmt = conn.prepareStatement(sql);)
+        {
+            stmt.setFloat(1,25);
+            stmt.setFloat(2,100);
+            stmt.setString(3,"nombre");
+
+            ResultSet rs = stmt.executeQuery();
+            while (rs.next()) {
+                String nombre = rs.getString("nombre");
+                String descripcion = rs.getString(2);
+                Float precio = rs.getFloat("precio");
+                listaProductos.add(new Productos(nombre, descripcion, precio));
+                System.out.println(nombre + " - " + descripcion + " - " + precio);
+            }
+        }catch (SQLException e) {
             System.out.println("¿salta la excepcion por aqui?");
             e.printStackTrace();
         }
 
-        //FASE DE CONSTRUCCIÓN
-        String sql = "SELECT nombre, descripcion, precio FROM productos";
-        PreparedStatement stmt = conn.prepareStatement(sql);
 
-        //FASE DE EJECUCIÓN
-        ResultSet rs = stmt.executeQuery();
-
-        //FASE DE PROCESO DE LA RESPUESTA
+        System.out.println("FIN DEL PROGRAMA");
 
 
-
-        //conn.close();
     }
 }
---
+++ src/UT8_AccesoABBDD/Productos.java
@@ -0,0 +1,15 @@
+package UT8_AccesoABBDD;
+
+public class Productos {
+    String nombre;
+    String descripcion;
+    Float precio;
+
+    public Productos(String nombre, String descripcion, Float precio) {
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+        this.precio = precio;
+    }
+
+
+}
