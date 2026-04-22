2241fc0ecd434d6aae0993a80c8b37931a5c2545
mario <mariomurc@gmail.com>
2026-04-22 12:29:10 +0200

    UT8_AccesoABBDD

--- src/UT8_AccesoABBDD/MainBBDD.java
+++ src/UT8_AccesoABBDD/MainBBDD.java
@@ -17,18 +17,21 @@ public class MainBBDD {
         String password = "alumno";
         ArrayList<Productos> listaProductos = new ArrayList<>();
         /// FASE DE CONEXIÓN
-
+        String columnaOrden = "nombre";
         //String sql = "SELECT nombre as n, descripcion, precio FROM productos";
+        String sql1 = "INSERT INTO productos (nombre, descripcion, precio) VALUES ('Camiseta de lANA', 'Camiseta de manga larga, 5% algodón', 59.99)";
+        System.out.println(sql1);
         String sql = "SELECT nombre,  descripcion, precio " +
                 "FROM productos " +
                 "WHERE precio > ? AND precio < ? "+
-                "ORDER BY ? ASC";
+                "ORDER BY " + columnaOrden ;
+        System.out.println(sql);
         try (Connection conn = conexion(url,user, password);
              PreparedStatement stmt = conn.prepareStatement(sql);)
         {
             stmt.setFloat(1,25);
             stmt.setFloat(2,100);
-            stmt.setString(3,"nombre");
+           // stmt.setString(3,"nombre");
 
             ResultSet rs = stmt.executeQuery();
             while (rs.next()) {
@@ -38,12 +41,28 @@ public class MainBBDD {
                 listaProductos.add(new Productos(nombre, descripcion, precio));
                 System.out.println(nombre + " - " + descripcion + " - " + precio);
             }
+
+
+
         }catch (SQLException e) {
             System.out.println("¿salta la excepcion por aqui?");
             e.printStackTrace();
         }
 
+        try (Connection conn = conexion(url,user, password);
+            PreparedStatement stmt = conn.prepareStatement(sql1, Statement.RETURN_GENERATED_KEYS);){
 
+            int reg = stmt.executeUpdate();
+            ResultSet rs = stmt.getGeneratedKeys();
+            System.out.println("FILAS AFECTADAS : " + reg);
+            if (rs.next()) System.out.println("ULTIMO ID INSERTADO : " + rs.getInt(1));
+
+
+
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
         System.out.println("FIN DEL PROGRAMA");
 
 
